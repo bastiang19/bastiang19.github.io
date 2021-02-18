b18329ca48f48947920187e4edb9a40d9adf50b8
Gleb <44839096+bastiang19@users.noreply.github.com>
2021-02-18 22:48:52 +0300

    Create About.vue

---
+++ src/components/About.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+      <section class="banner_area">
+<div class="banner_inner d-flex align-items-center">
+<div class="container">
+<div class="banner_content text-center">
+<h2>About me</h2>
+<div class="page_link">
+<router-link to="/"><a>Home</a></router-link>
+<router-link to="/about"><a>About</a></router-link>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section class="about_area section_gap">
+      <div class="container">
+        <div class="row justify-content-start align-items-center">
+          <div class="col-lg-5">
+            <div class="about_img">
+              <img class="" src="assets/img/about-us.png" alt="">
+            </div>
+          </div>
+          <div class="offset-lg-1 col-lg-5">
+            <div class="main_title text-left">
+              <h2>something 
+                <br>
+                about 
+                <br>
+                me
+              </h2>
+              <p>
+                Belarus. Minsk. 19 years. Turning the internet into art.
+              </p>
+              <p>
+                Cocktail of my skills: PHP, CSS, HTML, JavaScript, Jquery, Vue.js, Laravel, Xenforo, Mysql, Git, Linux, Node.js, Puppeteer, Playwright, NativeScript, React.js, Telegraf.js, ...
+              </p>
+              <a class="primary_btn" href="#">
+                <span onclick="window.location.href='https://t.me/bastiang19'">Write me
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
